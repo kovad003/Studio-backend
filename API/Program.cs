b4bd07825b64c80f7a945ffdc89bd6ed31274498
kovad003 <kovacsdansan@gmail.com>
2023-01-17 22:22:29 +0200

    Configured app
    - Simplified launchSettings.json
    - Configuring appsettings
    - Temporarily disabled Https

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// (Dependency Injection)
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -10,13 +10,14 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// (Middleware)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
