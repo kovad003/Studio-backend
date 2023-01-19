3d431064f8a9da37f4abfa9180ec4af080da4a81
kovad003 <kovacsdansan@gmail.com>
2023-01-19 20:31:38 +0200

    Refactoring
    - added ServiceExtension class to keep Program class thinner

---
+++ API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,33 @@
+using Application.Core;
+using Application.Projects;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddServiceExtensions(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+        });
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+            });
+        });
+        services.AddMediatR(typeof(List.Handler));
+        services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,4 @@
-using Application.Core;
-using Application.Projects;
-using MediatR;
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -9,15 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // (Dependency Injection)
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddMediatR(typeof(List.Handler));
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+builder.Services.AddServiceExtensions(builder.Configuration);
 
 var app = builder.Build();
 
